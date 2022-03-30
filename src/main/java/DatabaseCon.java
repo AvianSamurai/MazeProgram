c898e74ef7cefc5842e876f5204a40db05eb6e4f
Kyron <kyronw11@gmail.com>
2022-03-30 17:31:14 +1000

    added some skeliton code to DatabaseCon and MazeData

--- src/main/java/DatabaseCon.java
+++ src/main/java/DatabaseCon.java
@@ -1,3 +1,21 @@
 public class DatabaseCon {
 
+    /**
+     * Connects to the database and ensures that the correct
+     * structure has been created
+     */
+    public DatabaseCon() {
+        // Connect to the database
+
+        // Test the database structure
+    }
+
+    /**
+     * Get a list of mazes from the database
+     *
+     * @return
+     */
+    public MazeData[] GetMazes() {
+        return null;
+    }
 }
--- src/main/java/MazeData.java
+++ src/main/java/MazeData.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MazeData {
+public class MazeData {
+
+    private String name;
+    private String authorName;
+    private String creationDate;
+    private String lastModifiedData;
+
+    public MazeData(String name, String authorName, String creationDate, String lastModifiedDate, String MazeData) {
+        // Create maze data from arguments
+        this.name = name;
+        this.authorName = authorName;
+        this.creationDate = creationDate;
+        this.lastModifiedData = lastModifiedDate;
+
+        // Convert maze data to maze representation
+    }
 }

44c149ba514dd89736b3b2ce4226488bcceec668
HarryK <92285648+QUTIAB207GroupLHC@users.noreply.github.com>
2022-03-31 15:24:30 +1000

    Update MazeDB.java
    Made a rough skeleton for the MazeDB class. Needs functions filled out etc

--- src/main/java/MazeDB.java
+++ src/main/java/MazeDB.java
@@ -1,3 +1,49 @@
-public class MazeDB {
+import com.mysql.jdbc.Connection;
 
+import java.sql.*;
+import java.util.Properties;
+
+public class MazeDB {
+    private  String db_driver;
+    private  String db_url;
+    private  String username;
+    private  String pwd;
+
+    //Initialising a connection object to create a session with a specific database
+    private Connection connection;
+
+    //Properties object is used to maintain lists of values in which the key is a string and the value is a string
+    //I think also used to set new entries in a database?
+    private Properties properties;
+
+    //The statement object is used for executing sql statements
+    private Statement statement;
+
+    //get the properties or set properties from user
+    private Properties retrieveProps(){
+        return properties;
+    }
+
+    //Open a connection to the database
+    private Connection connection(){
+        return connection;
+    }
+
+    //Disconnect from database
+    public void disconnect(){
+
+    }
+
+    public ResultSet Query(String query) throws SQLException{
+
+        //return result from query
+        return null;
+    }
+
+    public int CreateUpdateDelete(String query) throws SQLException{
+
+        //Returns the number of deleted objects or edite columns/rows
+        return 0;
+    }
+    
 }

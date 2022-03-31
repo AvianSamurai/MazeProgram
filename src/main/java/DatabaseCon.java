77662470a8c56654c6b275121f3ecff452b5a63c
Kyron <kyronw11@gmail.com>
2022-03-31 11:48:56 +1000

    added code to make DatabaseCon() constructor read db.props

--- src/main/java/DatabaseCon.java
+++ src/main/java/DatabaseCon.java
@@ -1,20 +1,29 @@
-import java.utils.Properties;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.Properties;
 
 public class DatabaseCon {
 
     private final String propertiesFile = System.getProperty("user.dir") + "\\MazeProgram\\db.props";
-    private URL databaseURL;
+    private String databaseURL;
     private String schema;
     private String username, password;
 
     /**
      * Connects to the database and ensures that the correct
      * structure has been created
+     *
+     * @throws FileNotFoundException Thrown if db.props couldn't be found
      */
-    public DatabaseCon() {
-        // Get the database's properties
+    public DatabaseCon() throws FileNotFoundException {
+        // Get the database's properties [UNTESTED]
         Properties dbProps = new Properties();
         FileReader propsReader = new FileReader(propertiesFile);
+        databaseURL = dbProps.getProperty("jdbc.url");
+        schema = dbProps.getProperty("jdbc.schema");
+        username = dbProps.getProperty("jdbc.username");
+        password = dbProps.getProperty("jdbc.password");
 
         // Connect to the database
 

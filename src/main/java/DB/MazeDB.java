511e3d05e17b6e3b47e243ee26ea6f5784a6aefe
HarryK <hnskelly@gmail.com>
2022-04-01 13:20:45 +1000

    MazeDB class edit
    Changed the class to static, however only inner classes can be static. changed fields to private static as we only need to initialize the connection once. Added a skeleton method called 'Setup' at the bottom just in case we need it and took out the properties object declaration.

--- src/main/java/DB/MazeDB.java
+++ src/main/java/DB/MazeDB.java
@@ -5,25 +5,22 @@ import com.mysql.jdbc.Connection;
 import java.sql.*;
 import java.util.Properties;
 
-public class MazeDB {
-    private String db_driver;
-    private String db_url;
-    private String username;
-    private String pwd;
+//I looked up static classes in java and apparently you cant declare a class as static because
+//it is a top level class, but we can declare inner classes as static
+public static class MazeDB {
+    private static String db_driver;
+    private static String db_url;
+    private static String username;
+    private static String pwd;
 
     //Initialising a connection object to create a session with a specific database
     public Connection connection;
 
-    //Properties object is used to maintain lists of values in which the key is a string and the value is a string
-    //I think also used to set new entries in a database?
-    public Properties properties;
-
     //The statement object is used for executing sql statements
     public Statement statement;
 
-
     //Open a connection to the database
-    private Connection connection(){
+    public Connection connection(){
 
         return connection;
     }
@@ -43,5 +40,10 @@ public class MazeDB {
 
         //Returns the number of deleted objects or edite columns/rows
         return 0;
+    }
+
+    //Added a setup skeleton just in case
+    public void Setup(){
+
     }
 }

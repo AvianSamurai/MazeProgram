398634bab3271fed04579e66123455cef936d98e
Kyron <kyronw11@gmail.com>
2022-05-03 15:18:23 +1000

    Created maze datastruct and created a debug way to visualise the maze

--- src/main/java/Program/MazeFactory.java
+++ src/main/java/Program/MazeFactory.java
@@ -1,2 +1,15 @@
-package Program;public class MazeFactory {
+package Program;
+
+public class MazeFactory {
+    /**
+     * Creates a standard maze with no defined entry or exit
+     *
+     * @param width count of how many cells wide maze is to be
+     * @param height count of how many cells tall maze is to be
+     * @return a new MazeStructure object
+     */
+    public static MazeStructure CreateBasicMaze(int width, int height) {
+        MazeStructure m = new MazeStructure(width, height);
+        return m;
+    }
 }
--- src/main/java/Program/MazeStructure.java
+++ src/main/java/Program/MazeStructure.java
@@ -1,5 +1,15 @@
 package Program;
 
+import MazeGUI.MazeGUI;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.io.IOException;
+import java.nio.Buffer;
+
 public class MazeStructure {
     private int width, height;
     private I_Cell[][] cells;
@@ -8,5 +18,55 @@ public class MazeStructure {
         this.width = width;
         this.height = height;
         this.cells = new BasicCell[width][height];
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                cells[x][y] = new BasicCell();
+            }
+        }
+
+    }
+
+    /**
+     * Get the I_Cell at the given position in the maze
+     *
+     * @param x cell x position
+     * @param y cell y position
+     * @return reference to the I_Cell at that position
+     */
+    public I_Cell GetCellAtPosition(int x, int y) {
+        if(x > width - 1 || x < 0 || y > height - 1 || y < 0) {
+            throw new IndexOutOfBoundsException(String.format("Maze size is [{0}, {1}], Given index was [{2}, {3}]", width, height, x, y));
+        }
+        return cells[x][y];
+    }
+
+    public ImageIcon DebugDisplayMaze() {
+        BufferedImage bi = new BufferedImage(width * 32, height * 32, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = bi.createGraphics();
+        g.setBackground(Color.white);
+        g.fillRect(0, 0, width*32, height*32);
+        g.setColor(Color.BLACK);
+        g.setStroke(new BasicStroke(2));
+        ((BasicCell)cells[3][4]).CreateConnection((BasicCell)cells[4][4], I_Cell.Direction.EAST);
+        ((BasicCell)cells[4][4]).CreateConnection((BasicCell)cells[4][5], I_Cell.Direction.SOUTH);
+        ((BasicCell)cells[4][5]).CreateConnection((BasicCell)cells[4][6], I_Cell.Direction.SOUTH);
+        ((BasicCell)cells[4][6]).CreateConnection((BasicCell)cells[5][6], I_Cell.Direction.EAST);
+        for(int x = 0; x < width; x++) {
+            for(int y = 0; y < height; y++) {
+                if(((BasicCell)cells[x][y]).GetBorders()[0]) {
+                    g.drawLine(x*32, y*32, x*32 + 32, y*32);
+                }
+                if(((BasicCell)cells[x][y]).GetBorders()[1]) {
+                    g.drawLine(x*32 + 32, y*32, x*32 + 32, y*32 + 32);
+                }
+                if(((BasicCell)cells[x][y]).GetBorders()[2]) {
+                    g.drawLine(x*32, y*32 + 32, x*32 + 32, y*32 + 32);
+                }
+                if(((BasicCell)cells[x][y]).GetBorders()[3]) {
+                    g.drawLine(x*32, y*32, x*32, y*32 + 32);
+                }
+            }
+        }
+        return new ImageIcon(bi);
     }
 }

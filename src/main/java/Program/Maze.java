da9480c7955db41235c46235b7565f722d08b13c
Kyron <kyronw11@gmail.com>
2022-05-24 10:18:18 +1000

    Part one of manual merge to avoid unit testing deletion

--- src/main/java/Program/Maze.java
+++ src/main/java/Program/Maze.java
@@ -7,25 +7,30 @@ public class Maze {
     //Fields for the Maze object
     private String title;
     private String author;
-    private DateTimeFormatter theDateTime;
-    public int cellSize;
-    public int imageSize;
+    public DateTimeFormatter theDateTime;
+    public String cellSize;
+    public String imageSize;
     public String logo;
     private String type;
 
 
     //Constructor for the initialization of a new Maze object
-    public Maze (String title, String author, int cellSize, int imageSize, String logo){
+    public Maze (String title, String author, String cellSize, String type){
         this.title = title;
         this.author = author;
         this.cellSize = cellSize;
         this.imageSize = imageSize;
         this.logo = logo;
+        //this.imageSize = imageSize;
+        //this.logo = logo;
+        this.type = type;
+
+
     }
 
-    //Overloading constructor in case logo, title, and author is not entered in object call.
-    public Maze (int cellSize){
-        this.cellSize = cellSize;
+    public Maze (String title, String author){
+        this.title = title;
+        this.author = author;
     }
 
     //Retrieving private author String
@@ -38,12 +43,17 @@ public class Maze {
         return this.title;
     }
 
+    public String getCellSize(){return this.cellSize;}
+
+    public String getType(){return this.type;}
+
     //Retrieving the date and time of maze creation
     public String GetDateTime(){
         this.theDateTime = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
         LocalDateTime current = LocalDateTime.now();
         return this.theDateTime.format(current);
     }
+
 }
 
 

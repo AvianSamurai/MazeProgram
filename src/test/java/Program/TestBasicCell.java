1cbf1c1468d72d24bc897c3859ce7670b14ebb3f
JiaqiZhong <102640507+JiaqiZhong@users.noreply.github.com>
2022-05-25 12:30:45 +1000

    Update unit testing for BasicCell class
    Change variable name, fix the issue of comparing arrays by using assertArraryEquals

--- src/test/java/Program/TestBasicCell.java
+++ src/test/java/Program/TestBasicCell.java
@@ -3,16 +3,14 @@ package Program;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Arrays;
-
 public class TestBasicCell {
      BasicCell basicCell;
-     BasicCell otherCell;
+     BasicCell anotherCell;
 
     @BeforeEach
     void setup() {
         basicCell = new BasicCell();
-        otherCell = new BasicCell();
+        anotherCell = new BasicCell();
     }
 
     @Test
@@ -20,12 +18,8 @@ public class TestBasicCell {
     void testDeleteNorthBorder() {
         basicCell.SetBorders(new boolean[] {true, true, true, true});
         basicCell.SetBorder(Direction.NORTH, false);
-        assertEquals(false, basicCell.GetBorders()[0],
-                "North border should be deleted");
-        // Other 3 borders should remain unchanged
-        assertEquals(true, basicCell.GetBorders()[1]);
-        assertEquals(true, basicCell.GetBorders()[2]);
-        assertEquals(true, basicCell.GetBorders()[3]);
+        assertArrayEquals(new boolean[] {false, true, true, true}, basicCell.GetBorders(),
+                "North border should be deleted and other 3 borders should remain unchanged");
     }
 
     @Test
@@ -33,70 +27,45 @@ public class TestBasicCell {
     void testCreateNorthBorder() {
         basicCell.SetBorders(new boolean[] {false, false, false, false});
         basicCell.SetBorder(Direction.NORTH, true);
-        assertEquals(true, basicCell.GetBorders()[0],
-                "North border should be created");
-        // Other 3 directions don't have borders
-        assertEquals(false, basicCell.GetBorders()[1]);
-        assertEquals(false, basicCell.GetBorders()[2]);
-        assertEquals(false, basicCell.GetBorders()[3]);
+        assertArrayEquals(new boolean[] {true, false, false, false}, basicCell.GetBorders(),
+                "North border should be created and other 3 borders should remain with no borders");
     }
 
     @Test
     @DisplayName("Test connect one cell to another (left to right)")
     void testConnectLeftCellToRightCell() {
         basicCell.SetBorders(new boolean[] {true, true, true, true});
-        otherCell.SetBorders(new boolean[] {true, true, true, true});
-        basicCell.CreateConnection(otherCell, Direction.EAST);  // Should merge east border of left cell with west border of right cell
+        anotherCell.SetBorders(new boolean[] {true, true, true, true});
+        basicCell.CreateConnection(anotherCell, Direction.EAST);  // Should merge east border of left cell with west border of right cell
 
-        // Check basic cell borders after merging
-        assertEquals(false, basicCell.GetBorders()[1],
-                "East border should be deleted");
-        // Other 3 borders should remain unchanged
-        assertEquals(true, basicCell.GetBorders()[0]);
-        assertEquals(true, basicCell.GetBorders()[2]);
-        assertEquals(true, basicCell.GetBorders()[3]);
+        // Check borders of the basic cell after merging
+        assertArrayEquals(new boolean[] {true, false, true, true}, basicCell.GetBorders(),
+                "East border should be deleted and other 3 borders should remain unchanged");
 
-        // Check other cell borders after merging
-        assertEquals(false, otherCell.GetBorders()[3],
-                "West border should be deleted");
-        // Other 3 borders should remain unchanged
-        assertEquals(true, otherCell.GetBorders()[0]);
-        assertEquals(true, otherCell.GetBorders()[1]);
-        assertEquals(true, otherCell.GetBorders()[2]);
-
-        // Try to compare the actual array with an expected array directly,
-        // but they are stored in different memory, apply toString for comparison instead,
-        // may not be very reliable
-        /*
-        assertEquals("[true, false, true, true]", Arrays.toString(basicCell.GetBorders()),
-                "Other 3 borders should remain unchanged");
-        assertEquals("[true, true, true, false]", Arrays.toString(otherCell.GetBorders()),
-                "Other 3 borders should remain unchanged");
-         */
+        // Check borders of another cell after merging
+        assertArrayEquals(new boolean[] {true, true, true, false}, anotherCell.GetBorders(),
+                "West border should be deleted and other 3 borders should remain unchanged");
     }
 
     @Test
     @DisplayName("Test if a cell with no borders has any connections")
     void testCellWithNoBorders() {
         basicCell.SetBorders(false, false, false, false);
-        assertEquals(true, basicCell.hasAnyConnections(),
-                "The cell doesn't have any borders, should have connection to cells in 4 directions");
+        assertTrue(basicCell.hasAnyConnections(), "The cell doesn't have any borders, should have connection to cells in 4 directions");
     }
 
     @Test
     @DisplayName("Test if a cell with one border has any connections")
     void testCellWithOneBorder(){
         basicCell.SetBorders(false, false, false, true);
-        assertEquals(true, basicCell.hasAnyConnections(),
-                "The cell has a border, should still have connection to cells in other 3 directions");
+        assertTrue(basicCell.hasAnyConnections(), "The cell has a border, should still have connection to cells in other 3 directions");
     }
 
     @Test
     @DisplayName("Test if a cell with all borders has any connections")
     void testCellWithAllBorders() {
         basicCell.SetBorders(true, true, true, true);
-        assertEquals(false, basicCell.hasAnyConnections(),
-                "The cell has borders in every direction, can't connect to any other cells");
+        assertFalse(basicCell.hasAnyConnections(), "The cell has borders in every direction, can't connect to any other cells");
     }
 
     @Test
@@ -104,8 +73,7 @@ public class TestBasicCell {
     void testConnectionAfterDeleteABorder() {
         basicCell.SetBorders(true, true, true, true);
         basicCell.SetBorder(Direction.NORTH, false);
-        assertEquals(true, basicCell.hasAnyConnections(),
-                "The cell should has connection to north cell after north border is deleted");
+        assertTrue(basicCell.hasAnyConnections(), "The cell should has connection to north cell after north border is deleted");
     }
 
     @Test
@@ -119,10 +87,10 @@ public class TestBasicCell {
     @Test
     @DisplayName("Test if the cell is traversable")
     void testCellTraversable() {
-        assertEquals(true, basicCell.isPartOfMaze());
+        assertTrue(basicCell.isPartOfMaze());
     }
 
-    // Exceptional test case
+    // Exceptional test cases
     @Test
     @DisplayName("Test more than four borders")
     void testSetBorders() {
@@ -139,5 +107,10 @@ public class TestBasicCell {
         assertThrows(Exception.class, () -> {
             basicCell.SetBorders(threeBorders);
         });
+    }
+
+    @Test
+    void testGetCellImageRepresentation() {
+        // To be tested
     }
 }
